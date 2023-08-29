fa07b4b07bb09dbaa7ebd953a90728efb5044e1c
Alejandro Montes <95830903+AlepsMM03@users.noreply.github.com>
2023-08-28 19:41:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -31,21 +31,39 @@ function buscarFotoPorFecha() {
     }
 }
 
-function mostrarDatos({date, explanation, media_type, title, url}){
-     
+function mostrarDatos({date, explanation, media_type, title, url}) {
     const titulo = document.querySelector('#titulo');
-    titulo.innerHTML = title;
-    const fecha = document.querySelector('#fecha');
-    fecha.innerHTML = date;
-    const descripcion = document.querySelector('#descripcion');
-    descripcion.innerHTML = explanation;
-    const multimedia = document.querySelector('#c_multimedia');
-    if(media_type == 'video'){
-        multimedia.innerHTML = `<iframe class="embed-responsive-item" src="${url}"></iframe>`
+    if (title) {
+        titulo.innerHTML = title;
     } else {
-        multimedia.innerHTML = `<img src="${url}" class="img-fluid" alt="${url}">`
+        titulo.innerHTML = "";
+    }
+
+    const fecha = document.querySelector('#fecha');
+    if (date) {
+        fecha.innerHTML = date;
+    } else {
+        fecha.innerHTML = "";
+    }
+
+    const descripcion = document.querySelector('#descripcion');
+    if (explanation) {
+        descripcion.innerHTML = explanation;
+    } else {
+        descripcion.innerHTML = "";
+    }
+
+    const multimedia = document.querySelector('#c_multimedia');
+    
+    if (media_type === 'image' && url) {
+        multimedia.innerHTML = `<img src="${url}" class="img-fluid" alt="${title || 'Imagen'}">`;
+    } else if (media_type === 'video' && url) {
+        multimedia.innerHTML = `<iframe class="embed-responsive-item" src="${url}"></iframe>`;
+    } else {
+        multimedia.innerHTML = `<p>No se encontró una imagen e información para esta fecha. En ese entonces, los astronautas todavía estaban sacando "fotos" con telescopios de cartón.</p>`;
     }
 }
+
 // Obtener la fecha actual en formato YYYY-MM-DD
 const today = new Date();
 today.setHours(0, 0, 0, 0);
